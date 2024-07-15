d4243b777784b7cc06216000a373e4798eba5596
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-16 00:05:56 +0200

    installing tailwind css

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,26 @@
-import { Inter } from 'next/font/google';
+import { Josefin_Sans } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import '@/app/_styles/globals.css';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: 'Pokedex Explorer',
+  description:
+    'The Pokedex Explorer is a web application that allows users to explore various Pokémon, view detailed information about each, and manage their favorite Pokémon',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
-const HomePage = () => {
+const HomePage = async () => {
+  // const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+  // const data = await response.json();
+  // console.log(data);
+
   return <div>Pokedex Explorer</div>;
 };
 
