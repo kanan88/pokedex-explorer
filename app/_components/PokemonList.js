fe1e128860da5afc26879d530ef4c585a26f0467
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-16 23:04:03 +0200

    adding search filter functionality

--- app/_components/PokemonList.js
+++ app/_components/PokemonList.js
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState } from 'react';
+
 import { HiOutlineHeart, HiHeart } from 'react-icons/hi';
 
 import Image from 'next/image';
@@ -7,7 +9,20 @@ import Link from 'next/link';
 import { useLocalStorageState } from '../_hooks/useLocalStorageState';
 
 const PokemonList = ({ data, apiUrl, imageUrl }) => {
+  const [apiData, setApiData] = useState(data);
+
+  const [searchValue, setSearchValue] = useState('');
   const [favourites, setFavourites] = useLocalStorageState([], 'favourites');
+
+  const handleSearch = (value) => {
+    setSearchValue(value);
+
+    const updatedData = data?.filter((item) =>
+      item?.name?.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setApiData(updatedData);
+  };
 
   const onAddLike = (id, name) => {
     const checkPokemon = (obj) => obj.id.toString() === id.toString();
@@ -32,45 +47,63 @@ const PokemonList = ({ data, apiUrl, imageUrl }) => {
   };
 
   return (
-    <ul className="flex flex-wrap items-center justify-center gap-4 ">
-      {data?.map((item) => {
-        const id = item.url.replace(`${apiUrl}/pokemon/`, '').replace('/', '');
+    <>
+      <div className="flex justify-center">
+        <input
+          type="text"
+          class="h-14 w-96 pr-8 pl-5 rounded focus:outline-none text-primary-900"
+          placeholder="Filter pokemons by name..."
+          value={searchValue}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
 
-        return (
-          <li
-            key={item.url}
-            className="h-80 w-48 rounded-md mt-5 ml-5 bg-gray-300 border-primary-800 border "
-          >
-            <Link
-              href={`pokemon/${id}`}
-              className="h-80 w-48 p-5 flex flex-col items-center justify-between hover:bg-accent-300 rounded-md"
+      <ul className="flex flex-wrap items-center justify-center gap-4 ">
+        {apiData?.map((item) => {
+          const id = item.url
+            .replace(`${apiUrl}/pokemon/`, '')
+            .replace('/', '');
+
+          return (
+            <li
+              key={item.url}
+              className="h-80 w-48 rounded-md mt-5 ml-5 bg-gray-300 border-primary-800 border "
             >
-              <p className="text-gray-900 mb-5 text-xl ">{item.name}</p>
-              <Image
-                alt={item.name}
-                height={100}
-                width={100}
-                className="mb-5"
-                src={`${imageUrl}/${id}.svg`}
-              />
-              <p className="text-gray-900 text-lg">ID: {id}</p>
-            </Link>
+              <Link
+                href={`pokemon/${id}`}
+                className={`h-80 w-48 p-5 flex flex-col items-center justify-between hover:bg-accent-200 rounded-md ${
+                  favourites.find((fav) => fav.name === item.name)
+                    ? 'bg-accent-300'
+                    : ''
+                }`}
+              >
+                <p className="text-gray-900 mb-5 text-xl ">{item.name}</p>
+                <Image
+                  alt={item.name}
+                  height={100}
+                  width={100}
+                  className="mb-5"
+                  src={`${imageUrl}/${id}.svg`}
+                />
+                <p className="text-gray-900 text-lg">ID: {id}</p>
+              </Link>
 
-            {favourites.find((fav) => fav.name === item.name) ? (
-              <HiHeart
-                onClick={() => onRemoveLike(id)}
-                className="cursor-pointer text-3xl hover:scale-125 text-accent-300 hover:text-accent-300 transition-transform duration-200 ease-out"
-              />
-            ) : (
-              <HiOutlineHeart
-                onClick={() => onAddLike(id, item.name)}
-                className="cursor-pointer text-3xl hover:scale-125  hover:text-accent-300 transition-transform duration-200 ease-out"
-              />
-            )}
-          </li>
-        );
-      })}
-    </ul>
+              {favourites.find((fav) => fav.name === item.name) ? (
+                <HiHeart
+                  onClick={() => onRemoveLike(id)}
+                  className="cursor-pointer text-3xl hover:scale-125 text-accent-300 hover:text-accent-300 transition-transform duration-200 ease-out"
+                />
+              ) : (
+                <HiOutlineHeart
+                  onClick={() => onAddLike(id, item.name)}
+                  className="cursor-pointer text-3xl hover:scale-125  hover:text-accent-300 transition-transform duration-200 ease-out"
+                />
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
