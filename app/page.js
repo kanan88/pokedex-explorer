488f5c15e30cfd21a00ddf00be95fd3e3d272fd5
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-16 02:15:50 +0200

    adding first page limited on 20 pokemons

--- app/page.js
+++ app/page.js
@@ -1,9 +1,46 @@
-const HomePage = async () => {
-  // const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
-  // const data = await response.json();
-  // console.log(data);
+import Image from 'next/image';
+import Link from 'next/link';
 
-  return <div>Pokedex Explorer</div>;
+const BASE_API_URL = 'https://pokeapi.co/api/v2';
+const IMAGE_URL =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world';
+let OFFSET = 20;
+let LIMIT = 20;
+
+const HomePage = async () => {
+  const response = await fetch(
+    `${BASE_API_URL}/pokemon/?offset=${OFFSET}&limit=${LIMIT}`
+  );
+  const data = await response.json();
+
+  return (
+    <ul className="flex flex-wrap items-center justify-center gap-4">
+      {data?.results.map((item) => {
+        const id = item.url
+          .replace(`${BASE_API_URL}/pokemon/`, '')
+          .replace('/', '');
+
+        return (
+          <li key={item.url} className="h-80 rounded-md mt-5 ml-5 bg-gray-300 ">
+            <Link
+              href={`pokemon/${id}`}
+              className="p-5 flex flex-col items-center justify-between h-80"
+            >
+              <p className="text-gray-900 mb-5 text-lg">{item.name}</p>
+              <Image
+                alt={item.name}
+                height={100}
+                width={100}
+                className="mb-5"
+                src={`${IMAGE_URL}/${id}.svg`}
+              />
+              <p className="text-gray-900 text-lg">ID: {id}</p>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default HomePage;
---
+++ app/pokemon/[pokemonId]/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const PokemonDetailsPage = () => {
+  return (
+    <div>
+      <div>Pokemon Details</div>
+      <Link href="/">Return to Home Page</Link>
+    </div>
+  );
+};
+
+export default PokemonDetailsPage;
