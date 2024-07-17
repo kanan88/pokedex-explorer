3feca211ccc9136d3de2c050e115ffe474786be7
Kanan Farzali <kanan.farzali@gmail.com>
2024-07-17 11:55:35 +0200

    removing suspension from favourites page as there is no api data

--- app/favourites/page.js
+++ app/favourites/page.js
@@ -1,14 +1,7 @@
-import { Suspense } from 'react';
-
-import Spinner from '../_components/Spinner';
 import FavouritePokemonList from '../_components/FavouritePokemonList';
 
 const FavouritesPage = () => {
-  return (
-    <Suspense fallback={<Spinner />}>
-      <FavouritePokemonList />
-    </Suspense>
-  );
+  return <FavouritePokemonList />;
 };
 
 export default FavouritesPage;
